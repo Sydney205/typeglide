27a7a57f8d44247a09e63b2b8c2a5892e6bb163e
Sydney205 <diekechi@gmail.com>
2023-12-24 13:40:18 +0100

    refactor: formatted code

--- types/typeGlide.d.ts
+++ types/typeGlide.d.ts
@@ -1,14 +1,14 @@
-declare module 'typeglide' {
+declare module "typeglide" {
   interface TypeOptions {
     strings: string[];
-    typeSpeed?: number;
-    backSpeed?: number;
-    startDelay?: number;
-    backDelay?: number;
-    loop?: boolean;
-    loopCount?: number;
-    backspace?: boolean;
-    backspaceLastString?: boolean;
+    typeSpeed?: number | undefined;
+    backSpeed?: number | undefined;
+    startDelay?: number | undefined;
+    backDelay?: number | undefined;
+    loop?: boolean | undefined;
+    loopCount?: number | undefined;
+    backspace?: boolean | undefined;
+    backspaceLastString?: boolean | undefined;
   }
 
   function typeglide(options: TypeOptions): Promise<void>;
