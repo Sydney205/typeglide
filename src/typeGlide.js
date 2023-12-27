e1b0b7d8f4e7750e305c1da13cf35159ab76cca8
Sydney205 <diekechi@gmail.com>
2023-12-27 11:01:47 +0100

    feat: New feature added `Shuffle`

--- src/typeGlide.js
+++ src/typeGlide.js
@@ -1,37 +1,33 @@
-// Sleeper
 var { sleep } = require("../lib/util.js");
 
-/**
- *
- * TypeGlide - Typing effect on terminal
- *
- * @param {object} options - Object options for typeout
- * @param {string[]} options.strings - An array of strings to be typed.
- * @param {number} [options.typeSpeed=50] - The typing speed in milliseconds.
- * @param {number} [options.backSpeed=50] - The backspacing speed in milliseconds.
- * @param {number} [options.startDelay=100] - The delay before starting to type in milliseconds.
- * @param {number} [options.backDelay=2000] - The delay before backspacing in milliseconds.
- * @param {boolean} [options.loop=false] - Whether to loop through the strings.
- * @param {number} [options.loopCount=1] - The number of times to loop if looping is activated.
- * @param {boolean} [options.backspace=true] - Whether to backspace after each string.
- * @param {boolean} [options.backspaceLastString=true] - Whether to backspace after the last string.
- * @returns
- */
 function typeglide({
   strings,
   typeSpeed = 50,
-  backSpeed = 50,
+  backSpeed = 20,
   startDelay = 100,
   backDelay = 2000,
   loop = false,
   loopCount = 1,
   backspace = true,
   backspaceLastString = true,
+  shuffle = false,
 }) {
   return new Promise((resolve) => {
+
+    let shuffledStrings = strings
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+    
     async function typeString(index = 0) {
       if (index < strings.length) {
-        const string = strings[index];
+        if (!shuffle) {
+          var string = strings[index];
+        } else {
+          // Shuffle the strings if `shuffle` is activated
+          var string = shuffledStrings[index];
+        }
+
         let counter = 0;
 
         await sleep(startDelay);
@@ -47,7 +43,7 @@ function typeglide({
 
         if (backspace && !backspaceLastString && index == strings.length - 1) {
           process.stdout.write("\n");
-          // We exit to prevent backspace on the last string
+          // We exit to prevent backspacing the last string
           process.exit();
         }
 
