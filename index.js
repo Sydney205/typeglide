a7b566f31746985182029b3823c6b8f4831fa5cb
Sydney205 <diekechi@gmail.com>
2024-01-11 20:04:16 +0100

    modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @package TypeGlide - Create typing effect on terminal
  * @author Sydney205 <https://github.com/Sydney205>
- * @version 1.0.0
+ * @version 1.1.0
  * @url https://github.com/Sydney205/typeglide
  * @license MIT
  * @copyright (c) Dieke Sydney
