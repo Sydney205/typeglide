bfc9c642e0e95bbf65a918388681dbf74c7102c6
Sydney205 <diekechi@gmail.com>
2024-03-07 00:11:47 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
  * @package TypeGlide - Create typeglide effect on terminal
  * @author Sydney205 <https://github.com/Sydney205>
- * @version 1.1.3
+ * @version 1.2.3
  * @url https://github.com/Sydney205/typeglide
  * @license MIT
  * @copyright (c) Dieke Sydney
  *
  */
 
-const typeglide = require("./src/typeglide");
+const typeglide = require("./src/modules/typeglide");
 
 module.exports = typeglide;
